c8e321644b49db9660ae7d93d5679c34b08aab7c
replakcan <alper.makcan@gmail.com>
2025-07-21 16:20:46 +0300

    Changes leaderboard btn to navlink

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router";
+import { NavLink, Outlet, useNavigate } from "react-router";
 import "../styles/gameboard.css";
 
 export default function Root() {
@@ -74,7 +74,7 @@ export default function Root() {
   };
 
   const handleStartClick = () => {
-    navigate("game");
+    navigate("/game");
 
     if (intervalId) {
       clearInterval(intervalId);
@@ -90,10 +90,6 @@ export default function Root() {
     }, 1000);
 
     setIntervalId(id);
-  };
-
-  const handleLeaderboardClick = () => {
-    navigate("leaderboard");
   };
   return (
     <section className="root-section">
@@ -140,7 +136,7 @@ export default function Root() {
             <div>Time: {Math.floor(elapsedTime / 1000)} seconds</div>
           </>
         )}
-        <button onClick={handleLeaderboardClick}>Leaderboard</button>
+        <NavLink to="leaderboard">Leaderboard</NavLink>
       </header>
       <Outlet context={{ setCoords, targets, foundTargets }} />
     </section>
