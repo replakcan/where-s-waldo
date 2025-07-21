40f43ea085d76dd442508d362addb80d9274c311
replakcan <alper.makcan@gmail.com>
2025-07-21 15:45:35 +0300

    Creates browserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Test from "./test";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./routes.jsx";
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Test />
+    <RouterProvider router={router} />
   </StrictMode>
 );
