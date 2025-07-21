49c4aba1981108f4c4920f3406695c3b40e6e99a
replakcan <alper.makcan@gmail.com>
2025-07-21 18:03:43 +0300

    Creates leaderboard route

--- frontend/src/routes/leaderboard.jsx
+++ frontend/src/routes/leaderboard.jsx
@@ -1,3 +1,29 @@
-export default function LeaderBoard () {
-    
+import { useEffect, useState } from "react";
+import UserResult from "../components/user-result";
+import "../styles/leaderboard.css";
+
+export default function LeaderBoard() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch("http://localhost:3001/api/users");
+
+      const users = await res.json();
+
+      const orderedUsers = users.sort((a, b) => a.game_time - b.game_time);
+
+      setUsers(orderedUsers);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <section className="leaderboard">
+      {users?.map((user) => {
+        return <UserResult key={user.id} user={user} />;
+      })}
+    </section>
+  );
 }
